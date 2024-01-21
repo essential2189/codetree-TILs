94d9815f3df58f86046276cb94b6bf4e1acb4d7e
왕승재 <essential2189@gmail.com>
2024-01-21 23:20:42 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240121/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if (a % 3 == 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    if (a % 5 == 0) {
+        cout << '\n' << "YES";
+    } else {
+        cout << '\n' << "NO";
+    }
+}
