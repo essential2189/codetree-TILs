87611c28bf732f86e702e10d6eeef605805a6e8e
왕승재 <essential2189@gmail.com>
2024-01-22 17:57:00 +0900

    Title: 지그재그로 이동하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240122/지그재그로 이동하기/moving-with-zigzag.cpp
+++ 240122/지그재그로 이동하기/moving-with-zigzag.cpp
@@ -19,37 +19,19 @@ int main() {
 
     while (true) {
         if (a < b && a+distance >= b) {
-            cout << '\n';
-            cout << a << '+' << distance << '=' << a+distance << '\n';
-            cout << b  << ' ' << last << ' ' << lastDistance << '\n';
-            answer += abs(b - abs(last));
+            answer += abs(b - last);
             break;
         } else if (a > b && a+distance <= b) {
-            cout << '\n';
-            cout << a << '+' << distance << '=' << a+distance << '\n';
-            cout << b  << ' ' << last << ' ' << lastDistance << '\n';
             answer += abs(b - abs(last));
             break;
         }
 
-        cout << answer << '+' << lastDistance << '+';
-
         answer += lastDistance + abs((a + distance) - a);
         last = a + distance;
         lastDistance = abs((a + distance) - a);
-
-        cout << abs((a + distance) - a) << '=' << answer << '\n';
 
         distance *= -2;
     }
 
     cout << answer;
 }
-
-// 7+1=8
-// 7-2=5
-// 7+4=11
-// 7-8=-1
-
-
-// -1 0 1 2 3 4 5 6 7 8 9 10 11
