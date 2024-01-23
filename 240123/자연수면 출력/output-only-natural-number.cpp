a2e7b630f72ec695d19bfb592c9d803ae057341c
왕승재 <essential2189@gmail.com>
2024-01-23 12:19:17 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a > 0) {
+        while (b--) {
+            cout << a;
+        }
+    } else {
+        cout << 0;
+    }
+}
