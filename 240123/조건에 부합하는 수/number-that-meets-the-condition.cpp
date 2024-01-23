f640d12e0852b3c2d230c5396f0c0390880deffb
왕승재 <essential2189@gmail.com>
2024-01-23 12:30:30 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    for (int i = 0; i <= a; i++) {
+        if (i % 2 == 0 && i % 4 != 0){
+            continue;
+        }
+
+        if ((i / 8) % 2 == 0) {
+            continue;
+        }
+
+        if (i % 7 < 4) {
+            continue;
+        }
+
+        cout << i << ' ';
+    }
+}
