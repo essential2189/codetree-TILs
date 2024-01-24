785238d3bdc245455fbc5be624d05485c946f4cd
왕승재 <essential2189@gmail.com>
2024-01-24 12:17:55 +0900

    Title: 공약수의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    bool flag = false;
+    for (int i = a; i <= b; i++) {
+        if (1920 % i == 0 && 2880 % i == 0) {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+}
