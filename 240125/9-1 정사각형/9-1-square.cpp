5bcb2a93e1362a8b0b3e57f6689b394f65abc373
왕승재 <essential2189@gmail.com>
2024-01-25 14:52:31 +0900

    Title: 9-1 정사각형, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/9-1 정사각형/9-1-square.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a = 9;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << a;
+            a--;
+
+            if (a == 0) {
+                a = 9;
+            }
+        }
+        cout << '\n';
+    }
+}
