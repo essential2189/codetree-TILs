8c15acd29e2836113c5b5b86b354b681359b6582
왕승재 <essential2189@gmail.com>
2024-01-29 16:13:54 +0900

    Title: 알파벳 출력하기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240129/알파벳 출력하기 3/print-alphabet-3.cpp
+++ 240129/알파벳 출력하기 3/print-alphabet-3.cpp
@@ -7,8 +7,9 @@ int main() {
     int n;
     cin >> n;
 
-    string alpha = "ABCDEFGHIJKMNOPQRSTURWXYZ";
+    string alpha = "ABCDEFGHIJKLMNOPQRSTURWXYZ";
     int idx = 0;
+    
     for (int i = 0; i < n; i++) {
         for (int j = n; j > i; j--) {
             cout << alpha[idx];
