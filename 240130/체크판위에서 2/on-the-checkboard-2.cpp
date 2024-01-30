9d437a7511d322fcee2566c7b5bf8d5c1cd63d07
왕승재 <essential2189@gmail.com>
2024-01-30 22:42:11 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240130/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240130/체크판위에서 2/on-the-checkboard-2.cpp
@@ -7,28 +7,39 @@ int main() {
     int r, c;
     cin >> r >> c;
 
+    vector<vector<char>> board;
     vector<vector<int>> black;
 
     for (int i = 1; i <= r; i++) {
+        vector<char> v;
         for (int j = 1; j <= c; j ++) {
             char c;
             cin >> c;
+            v.push_back(c);
 
             if (i != r && j != c && c == 'B') {
                 black.push_back({i, j});
             }
         }
+        board.push_back(v);
+    }
+
+    if (board[0][0] != 'W' || board[r-1][c-1] != 'B') {
+        cout << 0;
+        return 0;
     }
 
     int answer = 0;
-    int secondWhite = 0;
     for (int i = 0; i < black.size(); i++) {
+        int secondWhite = 0;
         for (int y = black[i][0]+1; y < r; y++) {
             for (int x = black[i][1]+1; x < c; x++) {
-                secondWhite++;
+                if (board[y-1][x-1] == 'W') {
+                    secondWhite++;
+                }
             }
         }
-        answer += black.size() * secondWhite;
+        answer += secondWhite;
     }
 
     cout << answer;
