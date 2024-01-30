1f0d2654c13f252463f94b904973fdaac6f297ec
왕승재 <essential2189@gmail.com>
2024-01-30 23:01:23 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240130/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240130/체크판위에서 2/on-the-checkboard-2.cpp
@@ -8,38 +8,41 @@ int main() {
     cin >> r >> c;
 
     vector<vector<char>> board;
-    vector<vector<int>> black;
 
     for (int i = 1; i <= r; i++) {
         vector<char> v;
-        for (int j = 1; j <= c; j ++) {
+        for (int j = 1; j <= c; j++) {
             char c;
             cin >> c;
             v.push_back(c);
-
-            if (i != r && j != c && c == 'B') {
-                black.push_back({i, j});
-            }
         }
         board.push_back(v);
     }
 
-    if (board[0][0] != 'W' || board[r-1][c-1] != 'B') {
-        cout << 0;
-        return 0;
+    char startColor;
+    if (board[0][0] == 'W') {
+        startColor = 'W';
+    } else {
+        startColor = 'B';
     }
 
     int answer = 0;
-    for (int i = 0; i < black.size(); i++) {
-        int secondWhite = 0;
-        for (int y = black[i][0]+1; y < r; y++) {
-            for (int x = black[i][1]+1; x < c; x++) {
-                if (board[y-1][x-1] == 'W') {
-                    secondWhite++;
+    for (int i = 1; i < r-1; i++) {
+        for (int j = 1; j < c-1; j++) {
+
+            // first
+            if (board[i][j] != startColor) {
+                for (int a = i+1; a < r-1; a++) {
+                    for (int b = j+1; b < c-1; b++) {
+
+                        // second
+                        if (board[a][b] == startColor) {
+                            answer++;
+                        }
+                    }
                 }
             }
         }
-        answer += secondWhite;
     }
 
     cout << answer;
