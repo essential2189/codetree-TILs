4f84e742090f93ebe7b1a6efe9d02d41d003086b
왕승재 <essential2189@gmail.com>
2024-01-31 13:03:28 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240131/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> v;
+
+    while(n--) {
+        int x, y;
+        cin >> x >> y;
+
+        v.push_back({x, y});
+    }
+
+    int answer = 99999;
+
+    for (int i = 1; i < v.size()-1; i++) {
+        int temp = 0;
+        vector<int> curr = {v[0][0], v[0][1]};
+
+        for (int j = 0; j < v.size(); j++) {
+            if (i == j) {
+                continue;
+            }
+
+            temp += abs(curr[0] - v[j][0]) + abs(curr[1] - v[j][1]);
+            curr = {v[j][0], v[j][1]};
+        }
+
+        if (temp < answer) {
+            answer = temp;
+        }
+    }
+
+    cout << answer;
+}
