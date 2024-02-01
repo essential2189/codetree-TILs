b9f803bdbc67386dc555b5ad0fcba8abafd94c6f
왕승재 <essential2189@gmail.com>
2024-02-01 23:46:01 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240201/Date to Date/date-to-date.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int m1, d1, m2, d2;
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int answer = d2;
+
+    vector<int> month = {-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    for (int i = m1; i < m2; i++) {
+        if (i == m1) {
+            answer += month[i] - d1;
+        } else {
+            answer += month[i];
+        }
+    }
+
+    cout << answer + 1;
+}
