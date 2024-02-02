c19b00c378379648cf38824cb2bc8ae7051c3453
왕승재 <essential2189@gmail.com>
2024-02-02 20:04:15 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240202/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240202/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -13,7 +13,7 @@ int main() {
     sort(v.begin(), v.end());
 
     bool flag = true;
-    for (int i; i < v.size()-1; i++) {
+    for (int i = 0; i < v.size()-1; i++) {
         if (abs(v[i] - v[i+1]) != 1) {
             flag = false;
             break;
