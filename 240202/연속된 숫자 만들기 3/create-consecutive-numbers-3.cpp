fa37cd91c9b7c62e6888fbc83e5c2a3c661efd16
왕승재 <essential2189@gmail.com>
2024-02-02 22:48:53 +0900

    Title: 연속된 숫자 만들기 3, Time: 1093ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240202/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
+++ 240202/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
@@ -4,45 +4,52 @@
 
 using namespace std;
 
+bool check(vector<int> v) {
+    if (v[0] < v[1] && v[1] < v[2]) {
+        if (v[2] - v[1] == 1 && v[1] - v[0] == 1) {
+            return true;
+        }
+    } else if (v[0] > v[1] && v[1] > v[2]){
+        if (v[0] - v[1] == 1 && v[1] - v[2] == 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
 
+    if (check(vector<int>{a, b, c})){
+        cout << 0;
+        return 0;
+    }
+
     vector<int> v1 = {a, b, c, 0};
     vector<int> v2 = {a, b, c, 0};
 
-    int n = 10;
-    while (n--) {
+    while (true) {
         if (v1[1] < v1[2]) {
             v1 = {v1[0], v1[1]-1, v1[1], v1[3]+=1};
         } else if (v1[1] > v1[2]) {
             v1 = {v1[0], v1[1]+1, v1[1], v1[3]+=1};
         }
 
+        if (check(v1)) {
+            break;
+        }
+    }
+
+    while (true) {
         if (v2[0] < v2[1]) {
             v2 = {v2[1], v2[1]+1, v2[2], v2[3]+=1};
         } else if (v2[0] > v2[1]) {
             v2 = {v2[1], v2[1]-1, v2[2], v2[3]+=1};
         }
 
-        if (v1[0] < v1[1] && v1[1] < v1[2]) {
-            if (v1[2] - v1[1] == 1 && v1[1] - v1[0] == 1) {
-                break;
-            }
-        } else if (v1[0] > v1[1] && v1[1] > v1[2]){
-            if (v1[0] - v1[1] == 1 && v1[1] - v1[2] == 1) {
-                break;
-            }
-        }
-
-        if (v2[0] < v2[1] && v2[1] < v2[2]) {
-            if (v2[2] - v2[1] == 1 && v2[1] - v2[0] == 1) {
-                break;
-            }
-        } else if (v2[0] > v2[1] && v2[1] > v2[2]){
-            if (v2[0] - v2[1] == 1 && v2[1] - v2[2] == 1) {
-                break;
-            }
+        if (check(v2)) {
+            break;
         }
     }
 
