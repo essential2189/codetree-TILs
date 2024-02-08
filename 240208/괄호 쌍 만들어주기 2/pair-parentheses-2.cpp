37ff6a5a664ade72f595e0f63362b013e02073a2
왕승재 <essential2189@gmail.com>
2024-02-08 17:02:30 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    int answer = 0;
+    for (int i = 0; i < str.length()-2; i++) {
+        if (str[i] == '(' && str[i+1] == '(') {
+            for (int j = i+2; j < str.length()-1; j++) {
+                if (str[j] == ')' && str[j+1] == ')') {
+                    answer++;
+                }
+            }
+        }
+    }
+
+    cout << answer;
+ }
