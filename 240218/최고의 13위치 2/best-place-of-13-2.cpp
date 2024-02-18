1167c35c35f6f9617b0f9d4e3c32081e5bc6af04
왕승재 <essential2189@gmail.com>
2024-02-18 18:33:43 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240218/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240218/최고의 13위치 2/best-place-of-13-2.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+vector<vector<int>> rotation(vector<vector<int>> v) {
+    vector<vector<int>> temp(v.size(), vector<int>(v[0].size(), 0));
+
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v[0].size(); j++) {
+            temp[i][j] = v[v.size() - i - 1][v[0].size() - j - 1];
+        }
+    }
+
+    return temp;
+}
+
 int main() {
     int n;
     cin >> n;
@@ -20,33 +32,38 @@ int main() {
     }
 
     int answer = 0;
-    for (int a = 0; a < 2; a++) {
-        int max = 0;
-        int x, y;
-        bool flag = false;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n-2; j++) {
-                int sum = v[i][j] + v[i][j+1] + v[i][j+2];
-                if (sum > max) {
-                    max = sum;
-                    y = i;
-                    x = j;
-                    flag = true;
-                    break;
+    vector<vector<int>> temp_v = v;
+
+    for (int k = 0; k < 2; k++) {
+        int temp = 0;
+        for (int a = 0; a < 2; a++) {
+            int max = 0;
+            int x, y;
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n-2; j++) {
+                    int sum = temp_v[i][j] + temp_v[i][j+1] + temp_v[i][j+2];
+                    if (sum > max) {
+                        max = sum;
+                        y = i;
+                        x = j;
+                    }
                 }
             }
-            if (flag) {
-                break;
+
+            if (max > 0) {
+                temp_v[y][x] = -999;
+                temp_v[y][x+1] = -999;
+                temp_v[y][x+2] = -999;
+
+                temp += max;
             }
         }
 
-        if (max > 0) {
-            v[y][x] = -999;
-            v[y][x+1] = -999;
-            v[y][x+2] = -999;
-
-            answer += max;
+        if (temp > answer) {
+            answer = temp;
         }
+
+        temp_v = rotation(v);
     }
 
     cout << answer;
