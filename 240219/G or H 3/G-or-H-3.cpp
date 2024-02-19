05998cd1a903390a9b3154cef6ef6f8de6ae9595
왕승재 <essential2189@gmail.com>
2024-02-19 16:49:03 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240219/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<pair<int, char>> v;
+
+    while (n--) {
+        int a;
+        char c;
+        cin >> a >> c;
+        v.push_back({a, c});
+    }
+
+    sort(v.begin(), v.end());
+
+    int answer = 0;
+    for (int i = 0; i < v.size(); i++) {
+        int sum = 0;
+        for (int j = i; j < v.size(); j++) {
+            if (v[j].first <= i+k+1) {
+                if (v[j].second == 'G') {
+                    sum += 1;
+                } else if (v[j].second == 'H') {
+                    sum += 2;
+                }
+            }
+        }
+
+        if (sum > answer) {
+            answer = sum;
+        }
+    }
+
+    cout << answer;
+}
