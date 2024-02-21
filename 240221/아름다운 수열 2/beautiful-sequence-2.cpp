7f4c744aa641b76cecb50205f80a22862c0b1e00
왕승재 <essential2189@gmail.com>
2024-02-21 20:23:49 +0900

    Title: 아름다운 수열 2, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240221/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool contain(vector<vector<int>> v, vector<int> i) {
+    for (vector<int> vv: v) {
+        if (vv == i) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a;
+    vector<int> b;
+
+    for (int k = 0; k < n; k++) {
+        int i;
+        cin >> i;
+        a.push_back(i);
+    }
+    for (int k = 0; k < m; k++) {
+        int i;
+        cin >> i;
+        b.push_back(i);
+    }
+
+    vector<vector<int>> per;
+    do {
+        per.push_back(b);
+    } while(next_permutation(b.begin(), b.end()));
+
+
+    int answer = 0;
+    for (int i = 0; i <= a.size()-m; i++) {
+        if (contain(per, vector<int>(a.begin()+i, a.begin()+i+m))) {
+            answer++;
+        }
+    }
+
+    cout << answer;
+}
