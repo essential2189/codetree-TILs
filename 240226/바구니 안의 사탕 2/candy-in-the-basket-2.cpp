c8fbc59d222392782a59ad3868e120ae7e0be543
왕승재 <essential2189@gmail.com>
2024-02-26 22:25:22 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240226/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 240226/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -7,23 +7,25 @@ using namespace std;
 int main() {
     int n, k;
     cin >> n >> k;
-    
-    vector<int> v(k*2*2, 0);
 
+    vector<int> v(100, 0);
     for (int i = 0; i < n; i++) {
-        int candy, b;
-        cin >> candy >> b;
-        v[b] += candy;
+        int candy, a;
+        cin >> candy >> a;
+        v[a] += candy;
     }
 
     int answer = 0;
-    for (int i = k; i < k*2; i++) {
-        int sum = accumulate(v.begin()+i-k, v.begin()+i+k+1, 0);
+    for (int i = 0; i <= 100; i++) {
+        int end = i+k+k;
+        if (i+k+k > 100) {
+            end = 100;
+        }
+        int sum = accumulate(v.begin()+i, v.begin()+end+1, 0);
 
         if (sum > answer) {
             answer = sum;
         }
     }
-
     cout << answer;
 }
