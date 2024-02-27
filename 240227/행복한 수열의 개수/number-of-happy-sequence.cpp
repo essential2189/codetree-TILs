ce8c67cf14851befef72cd1b351561135f015119
왕승재 <essential2189@gmail.com>
2024-02-27 20:36:29 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> v;
+    for (int i = 0; i < n; i++) {
+        vector<int> temp;
+        for (int j = 0; j < n; j++) {
+            int a;
+            cin >> a;
+            temp.push_back(a);
+        }
+        v.push_back(temp);
+    }
+
+    int answer = 0;
+    for (int i = 0; i < n; i++) {
+        bool h_flag = true;
+        bool v_flag = true;
+        int count_h = 1;
+        int count_v = 1;
+        for (int j = 0; j < n-1; j++) {
+            if (v[i][j] == v[i][j+1]) {
+                count_h++;
+            } else {
+                count_h = 1;
+            }
+            if (v[j][i] == v[j+1][i]) {
+                count_v++;
+            } else {
+                count_v = 1;
+            }
+            
+            if (h_flag && count_h >= m) {
+                answer++;
+                h_flag = false;
+            }
+            if (v_flag && count_v >= m) {
+                answer++;
+                count_v = 1;
+                v_flag = false;
+            }
+        }
+    }
+
+    cout << answer;
+}
