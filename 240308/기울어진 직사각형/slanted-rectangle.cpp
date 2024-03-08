34dbc8ddbf43307fc7e04ebd37eac818939f7937
왕승재 <essential2189@gmail.com>
2024-03-08 13:37:37 +0900

    Title: 기울어진 직사각형, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240308/기울어진 직사각형/slanted-rectangle.cpp
+++ 240308/기울어진 직사각형/slanted-rectangle.cpp
@@ -43,13 +43,13 @@ vector<int> second(vector<vector<int>> v, int i, int j) {
     return {last_y, last_x, count};
 }
 
-vector<int> third(vector<vector<int>> v, int i, int j, int start_i, int start_j) {
+vector<int> third(vector<vector<int>> v, int i, int j, int start_i) {
     int count = 0;
     int idx = 1;
     int last_y = -1;
     int last_x = -1;
     while(true) {
-        if (i + idx > start_i || j - idx < start_j) {
+        if (i + idx > start_i || j - idx < 0) {
             break;
         }
         count += v[i+idx][j-idx];
@@ -94,13 +94,24 @@ int main() {
         }
         v.push_back(temp);
     }
+    // vector<int> f = first(v, 4, 2);
+    // cout <<"first "<< f[0] << ' ' << f[1] << ' ' << f[2] << '\n';
+
+    // vector<int> s = second(v, f[0], f[1]);
+    // cout <<"second "<< s[0] << ' ' << s[1] << ' ' << s[2] << '\n';
+
+    // vector<int> t = third(v, s[0], s[1], n-2-1);
+    // cout <<"third "<< t[0] << ' ' << t[1] << ' ' << t[2] << '\n';
+
+    // vector<int> fo = fourth(v, t[0], t[1], 4, 2);
+    // cout <<"fourth "<< fo[0] << ' ' << fo[1] << ' ' << fo[2] << '\n';
 
     int answer = 0;
     for (int i = 2; i < n; i++) {
         for (int j = 1; j < n-1; j++) {
             vector<int> f = first(v, i, j);
             vector<int> s = second(v, f[0], f[1]);
-            vector<int> t = third(v, s[0], s[1], n-j-1, n-i-1);
+            vector<int> t = third(v, s[0], s[1], n-j-1);
             vector<int> fo = fourth(v, t[0], t[1], i, j);
 
             int sum = f[2] + s[2] + t[2] + fo[2];
