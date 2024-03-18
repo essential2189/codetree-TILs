3cbb54c794f85bab6901435b7a13c12853129c40
왕승재 <essential2189@gmail.com>
2024-03-18 22:02:12 +0900

    Title: 1이 3개 이상 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240318/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,30 @@
+def solution(n):
+    board = []
+    for _ in range(n):
+        board.append(list(map(int, input().split(" "))))
+
+    dist = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+    answer = 0
+
+    for i in range(n):
+        for j in range(n):
+            count = 0
+
+            for d in dist:
+                y = i + d[0]
+                x = j + d[1]
+
+                if y >= 0 and y < n and x >= 0 and x < n:
+                    if board[y][x] == 1:
+                        count += 1
+                    
+                    if count >= 3:
+                        answer += 1
+                        break
+
+    return answer
+
+n = int(input())
+
+print(solution(n))
