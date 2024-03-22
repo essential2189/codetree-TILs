77b63d86360a6922053b7f90588dffcf9a4aeea0
왕승재 <essential2189@gmail.com>
2024-03-22 12:41:49 +0900

    Title: 되돌아오기 2, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240322/되돌아오기 2/come-back-2.py
@@ -0,0 +1,38 @@
+c = input()
+
+def turn_left(d):
+    direct = [[1, 0], [0, -1], [-1, 0], [0, 1]]
+
+    idx = direct.index(d)
+    
+    return direct[(idx + 1) % 4]
+
+def turn_right(d):
+    direct = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+    idx = direct.index(d)
+    
+    return direct[(idx + 1) % 4]
+
+
+def solution(c):
+    start = [0, 0]
+    direct = [1, 0]
+
+    answer = 0
+    for s in c:
+        if s == "F":
+            start[0] += direct[0]
+            start[1] += direct[1]
+        elif s == "L":
+            direct = turn_left(direct)
+        elif s == "R":
+            direct = turn_right(direct)
+
+        answer += 1
+        
+        if start == [0, 0]:
+            return answer
+
+
+print(solution(c))
