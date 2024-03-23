0295770621e9fc159c775a3a0aa931320b59c7dd
왕승재 <essential2189@gmail.com>
2024-03-23 16:06:44 +0900

    Title: 거울에 레이저 쏘기 2, Time: 247ms, Memory: 48MB, Status: Wrong Answer - CodeTree

---
+++ 240323/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,57 @@
+n = int(input())
+
+board = [list(input()) for _ in range(n)]
+
+k = int(input())
+
+
+def solution(n, board, k):
+    k -= 1
+    i = k // n
+
+    direct = [0, 0]
+    location = [0, 0]
+    if i == 0:
+        direct = [1, 0]
+        location = [0, k % n]
+    elif i == 1:
+        direct = [0, -1]
+        location = [k % n, n-1]
+    elif i == 2:
+        direct = [-1, 0]
+        location = [n-1, n - 1 - k % n]
+    elif i == 3:
+        direct = [0, 1]
+        location = [k % n, 0]
+
+    answer = 1
+    while(True):
+        if board[location[0]][location[1]] == '/':
+            if direct == [1, 0]:
+                direct = [0, -1]
+            elif direct == [-1, 0]:
+                direct = [0, 1]
+            elif direct == [0, 1]:
+                direct = [-1, 0]
+            elif direct == [0, -1]:
+                direct = [11, 0]
+        else:
+            if direct == [1, 0]:
+                direct = [0, 1]
+            elif direct == [-1, 0]:
+                direct = [0, -1]
+            elif direct == [0, 1]:
+                direct = [1, 0]
+            elif direct == [0, -1]:
+                direct = [-1, 0]
+
+        location[0] += direct[0]
+        location[1] += direct[1]
+
+        if location[0] < 0 or location[0] >= n or location[1] < 0 or location[1] >= n:
+            return answer
+        
+        answer += 1
+
+
+print(solution(n, board, k))
