e47fe6bea65584a55b5a97575abe967a749fd9c4
왕승재 <essential2189@gmail.com>
2024-03-24 18:56:03 +0900

    Title: 007, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240324/007/007.py
@@ -0,0 +1,30 @@
+class ZeroZeroSeven:
+    def __init__(self):
+        self.secret_code = ""
+        self.meeting_point = ""
+        self.time = 0
+    
+    def set_secret_code(self, code):
+        self.secret_code = code
+    
+    def set_meeting_point(self, mp):
+        self.meeting_point = mp
+    
+    def set_time(self, time):
+        self.time = time
+    
+    def print(self):
+        print("secret code : ", self.secret_code)
+        print("meeting point : ", self.meeting_point)
+        print("time : ", self.time)
+
+    
+a = input().split(" ")
+
+zzs = ZeroZeroSeven()
+
+zzs.set_secret_code(a[0])
+zzs.set_meeting_point(a[1])
+zzs.set_time(a[2])
+
+zzs.print()
