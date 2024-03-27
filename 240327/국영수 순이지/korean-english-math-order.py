8b7822acaaacedb28bb49f33eec2cc77e81045e1
왕승재 <essential2189@gmail.com>
2024-03-27 12:32:20 +0900

    Title: 국영수 순이지, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+class Student:
+    def __init__(self, name, k, e, m):
+        self.name = name
+        self.k = k
+        self.e = e
+        self.m = m
+
+    def print(self):
+        print(self.name, self.k, self.e, self.m)
+
+arr = []
+for _ in range(n):
+    name, k, e, m = list(input().strip().split(" "))
+
+    arr.append(Student(name, k, e, m))
+
+arr.sort(lambda x: (x.k, x.e, x.m))
+
+for a in arr:
+    a.print()
