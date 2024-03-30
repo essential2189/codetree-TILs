98199bf637eb510b8a38363cde5156591922ffed
왕승재 <essential2189@gmail.com>
2024-03-31 01:19:29 +0900

    Title: 최고의 33위치, Time: 110ms, Memory: 25MB, Status: Passed - CodeTree

--- 240331/최고의 33위치/best-place-of-33.py
+++ 240331/최고의 33위치/best-place-of-33.py
@@ -5,7 +5,7 @@ board = [list(map(int, input().strip().split(" "))) for _ in range(n)]
 answer = 0
 for i in range(n-2):
     for j in range(n-2):
-        sum_ = sum(sum(board[i:i+3][j:j+3], []))
+        sum_ = sum(sum([row[j:j+3] for row in board[i:i+3]], []))
         
         if sum_ > answer:
             answer = sum_
