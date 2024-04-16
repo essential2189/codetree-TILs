62f9a739a3f19a3549915ad00b5a9d97c400df6d
왕승재 <essential2189@gmail.com>
2024-04-16 12:51:34 +0900

    Title: 숫자 등장 횟수, Time: 184ms, Memory: 34MB, Status: Passed - CodeTree

--- 240416/숫자 등장 횟수/number-frequency.py
+++ 240416/숫자 등장 횟수/number-frequency.py
@@ -3,10 +3,16 @@ n, m = list(map(int, input().strip().split(" ")))
 list1 = list(map(int, input().strip().split(" ")))
 list2 = list(map(int, input().strip().split(" ")))
 
-for l2 in list2:
-    count = 0
-    for l1 in list1:
-        if l2 == l1:
-            count += 1
+dict_ = {}
+for l1 in list1:
+    if l1 in dict_:
+        dict_[l1] += 1
+    else:
+        dict_[l1] = 1
     
-    print(count, end=" ")
+
+for l2 in list2:
+    if l2 in dict_:
+        print(dict_[l2], end=" ")
+    else:
+        print(0, end=" ")
