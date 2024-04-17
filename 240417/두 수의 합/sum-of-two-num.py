e745058ad3a869d43b25d0f1c4ffb7fe27b50cef
왕승재 <essential2189@gmail.com>
2024-04-17 12:32:47 +0900

    Title: 두 수의 합, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240417/두 수의 합/sum-of-two-num.py
+++ 240417/두 수의 합/sum-of-two-num.py
@@ -3,9 +3,13 @@ n, k = list(map(int, input().strip().split(" ")))
 l = list(map(int, input().strip().split(" ")))
 
 answer = 0
+m = {}
 for i in range(len(l)-1):
-    for j in range(i+1, len(l)):
-        if l[i]+l[j] == k:
-            answer += 1
+    if l[i] not in m:
+        for j in range(i+1, len(l)):
+            if l[i] + l[j] == k:
+                answer += 1
+    m[l[i]] = True
+
 
 print(answer)
