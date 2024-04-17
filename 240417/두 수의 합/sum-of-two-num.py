0888920f936c76155e039bbd2f384e7bf16c6592
왕승재 <essential2189@gmail.com>
2024-04-17 12:22:09 +0900

    Title: 두 수의 합, Time: 5045ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 240417/두 수의 합/sum-of-two-num.py
+++ 240417/두 수의 합/sum-of-two-num.py
@@ -4,7 +4,7 @@ l = list(map(int, input().strip().split(" ")))
 
 answer = 0
 for i in range(len(l)-1):
-    for j in range(i, len(l)):
+    for j in range(i+1, len(l)):
         if l[i]+l[j] == k:
             answer += 1
 
