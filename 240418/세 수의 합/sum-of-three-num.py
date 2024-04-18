30abd88f4f12f84d7b4dfff47c8b7330b2ea8f0c
왕승재 <essential2189@gmail.com>
2024-04-18 12:38:33 +0900

    Title: 세 수의 합, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

--- 240418/세 수의 합/sum-of-three-num.py
+++ 240418/세 수의 합/sum-of-three-num.py
@@ -3,7 +3,6 @@ n, k = list(map(int, input().strip().split(" ")))
 arr = list(map(int, input().strip().split(" ")))
 
 m = {}
-
 for a in arr:
     if a in m:
         m[a] += 1
@@ -11,7 +10,7 @@ for a in arr:
         m[a] = 1
 
 answer = 0
-for i in range(k):
+for i in range(n):
     m[arr[i]] -= 1
     for j in range(i):
         diff = k - arr[i] - arr[j]
