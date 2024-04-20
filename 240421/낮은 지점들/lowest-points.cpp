09419c803aa167470dd238680c3860c01640cac0
왕승재 <essential2189@gmail.com>
2024-04-21 00:58:47 +0900

    Title: 낮은 지점들, Time: 63ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240421/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    map<int, int> m;
+
+    while(n--) {
+        int x, y;
+        cin >> x >> y;
+
+        if (m.find(x) != m.end()) {
+            if (y < m[x]) {
+                m[x] = y;
+            }
+        } else {
+            m[x] = y;
+        }
+    }
+
+    int answer = 0;
+    for (auto i: m) {
+        answer += i.second;
+    }
+
+    cout << answer;
+}
